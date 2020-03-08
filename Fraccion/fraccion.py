75294ba9ea5a033e4da454c51c9b2b97475a16af
Maxi Chaves <maximiliano.e.chaves@gmail.com>
2020-03-08 19:58:18 -0300

    Primer commit

---
+++ Fraccion/fraccion.py
@@ -0,0 +1,7 @@
+class Fraccion:
+	numerador = None
+	denominador = None
+
+	# def __init__ (self, num, den):
+	# 	self.numerador = num
+	# 	self.denominador = den
---
+++ Fraccion/principal.py
@@ -0,0 +1,6 @@
+from fraccion import Fraccion
+
+fraccion1 = Fraccion()
+
+fraccion1.numerador =20
+print (fraccion1.numerador)
