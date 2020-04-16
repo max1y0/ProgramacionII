443b0b0b014a86273521ee91e1132b09024d97b7
Maxi Chaves <maximiliano.e.chaves@gmail.com>
2020-04-16 20:38:58 -0300

    Agrego metodo sumar

--- Fraccion/fraccion.py
+++ Fraccion/fraccion.py
@@ -6,3 +6,13 @@ class Fraccion:
 
     def mostrar (self):
         print (self.numerador , "/" , self.denominador)
+
+    def sumar (self, fraccion2):
+        #inicializo la variable resultado en 1 / 1. Si no se inicializa hay error
+        resultado = Fraccion(1,1)
+
+        #metodo mariposa
+        resultado.numerador = self.numerador * fraccion2.denominador + fraccion2.numerador * self.denominador
+        resultado.denominador = self.denominador * fraccion2.denominador
+        return resultado
+
--- Fraccion/principal.py
+++ Fraccion/principal.py
@@ -1,5 +1,9 @@
 from fraccion import Fraccion
 
 fraccion1 = Fraccion(2,3)
+fraccion2 = Fraccion(4,5)
 
-fraccion1.mostrar()
+resultado = fraccion1.sumar(fraccion2)
+
+resultado.mostrar()
+
