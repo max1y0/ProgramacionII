c05513fcd99b9a0fa1c930dce57e5e5c80999a7a
Maxi Chaves <maximiliano.e.chaves@gmail.com>
2020-05-08 17:47:36 -0300

    Agrego metodos division y multiplicacion

--- Fraccion/fraccion.py
+++ Fraccion/fraccion.py
@@ -5,14 +5,38 @@ class Fraccion:
         self.denominador = den
 
     def mostrar (self):
-        print (self.numerador , "/" , self.denominador)
+        print (self.numerador,"/",self.denominador)
 
     def sumar (self, fraccion2):
-        #inicializo la variable resultado en 1 / 1. Si no se inicializa hay error
+        #inicializo la variable resultado
         resultado = Fraccion(1,1)
-
         #metodo mariposa
         resultado.numerador = self.numerador * fraccion2.denominador + fraccion2.numerador * self.denominador
         resultado.denominador = self.denominador * fraccion2.denominador
         return resultado
+
+    def resta (self, fraccion2):
+        #inicializo la variable resultado
+        resultado = Fraccion(1,1)
+        #metodo mariposa para restas
+        resultado.numerador = self.numerador * fraccion2.denominador - fraccion2.numerador * self.denominador
+        resultado.denominador = self.denominador * fraccion2.denominador
+        return resultado
+   
+    def division (self, fraccion2):
+        #inicializo la variable resultado
+        resultado = Fraccion(1,1)
+        #división
+        resultado.numerador = self.numerador * fraccion2.denominador
+        resultado.denominador = self.denominador * fraccion2.numerador
+        return resultado
+
+    def multiplicacion (self, fraccion2):
+        #inicializo la variable resultado 
+        resultado = Fraccion(1,1)
+        #multiplico
+        resultado.numerador = self.numerador * fraccion2.numerador
+        resultado.denominador = self.denominador * fraccion2.denominador
+        return resultado
+
 
--- Fraccion/principal.py
+++ Fraccion/principal.py
@@ -3,7 +3,16 @@ from fraccion import Fraccion
 fraccion1 = Fraccion(2,3)
 fraccion2 = Fraccion(4,5)
 
-resultado = fraccion1.sumar(fraccion2)
+resultadoSuma = fraccion1.sumar(fraccion2)
+resultadoResta = fraccion1.resta(fraccion2)
+resultadoMulti = fraccion1.multiplicacion(fraccion2)
+resultadoDiv = fraccion1.division(fraccion2)
 
-resultado.mostrar()
-
+print("la suma es:")
+resultadoSuma.mostrar()
+print("La resta es:")
+resultadoResta.mostrar()
+print("la multiplicacion es:")
+resultadoMulti.mostrar()
+print("La division es:")
+resultadoDiv.mostrar()
