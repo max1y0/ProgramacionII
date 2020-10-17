e0a0bdce4a006b5d2d5cd537cf31915e14960224
Maximiliano Chaves <maximiliano.e.chaves@gmail.com>
2020-10-17 20:17:18 -0300

    Update producto.py
    Actualizo para etapa 2

--- adivinarPrecio/producto.py
+++ adivinarPrecio/producto.py
@@ -1,2 +1,11 @@
-from random import randrange
+from random import randrange,choices
+
 class Producto:
+  nombre = ""
+  precio = 0
+
+  def generarPrecio(self):
+    #Su codigo de generar precio va aca
+  
+  def def generarNombre(self):
+    #su codigo de generar nombre va aca
