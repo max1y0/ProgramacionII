e0eadc5fa6160f3a38bb48157ae33234241559cd
Maximiliano Chaves <maximilianochaves@Maximilianos-MacBook-Air.local>
2022-05-15 22:14:34 -0300

    Actualizo con lo realizado en la ultima clase

--- clicker/script.js
+++ clicker/script.js
@@ -2,12 +2,15 @@ let contador = 0
 let cursor = {
 	cantidad: 0,
 	CPS: 0.1,
-	precio:15
+	precio:15,
+	precioMejora: 50
 }
 
 document.getElementById('contador').innerHTML = contador
 document.getElementById('boton').addEventListener("click", aumentar);
 document.getElementById('cursor').addEventListener("click", comprarCursor);
+document.getElementById('mejoraCursor').addEventListener("click", mejorarCursor);
+
 
 
 function aumentar(){
@@ -24,3 +27,22 @@ function comprarCursor(){
 		document.getElementById('cursor').innerHTML = 'Cursor ' + cursor.precio
 	}
 }
+
+function mejorarCursor(){
+	if ( contador > cursor.precioMejora ) {
+		contador = contador - cursor.precioMejora
+		cursor.precioMejora = Math.floor(cursor.precioMejora * 1.8)
+		cursor.CPS = cursor.CPS * 1.5
+		document.getElementById('contador').innerHTML = contador
+		document.getElementById('mejoraCursor').innerHTML = 'Mejorar cursor CPS: ' + cursor.CPS.toFixed(2) + ' = ' + cursor.precioMejora
+
+	}
+}
+
+function autoclick(){
+	//
+}
+
+window.setInterval(function(){
+    autoclick();
+}, 1000);
