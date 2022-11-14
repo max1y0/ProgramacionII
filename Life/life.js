695ecd8f5617d653f24f8bc82862c626fdc9024c
Maxi <maximiliano.e.chaves@gmail.com>
2022-11-14 13:26:16 -0300

    actualizo cuenta vecinos, esquinas y paredes

--- Life/life.js
+++ Life/life.js
@@ -10,28 +10,14 @@ class Celula {
     if (this.viva == 0) {
       fill(0);
     } else {
-      
-      if (this.paredSup()) {
-        fill(200, 20, 140);
-      }
-      if (this.paredInf()) {
-        fill(50, 20, 140);
-      }
-      if (this.paredDer()) {
-        fill(50, 220, 140);
-      }
-      if (this.paredIzq()) {
-        fill(50, 120, 140);
-      }
-      if(!( this.paredDer() || this.paredIzq() || this.paredSup() || this.paredInf())){
-      fill(250);
-      }
+      fill(this.x*16, this.y*3+120, 140);
+
       strokeWeight(0.5);
     }
     rect(this.x * this.tam, this.y * this.tam, this.tam, this.tam);
     fill(0);
-
-    text(this.vecinos, this.x * 10, (this.y + 1) * 10);
+    textSize(escala);
+    text(this.vecinos, this.x * escala, (this.y + 1) * escala);
   }
 
   paredSup() {
@@ -41,7 +27,7 @@ class Celula {
   }
 
   paredInf() {
-    if (this.y == height / 10 - 1) {
+    if (this.y == height / escala - 1) {
       return true;
     }
   }
@@ -53,33 +39,69 @@ class Celula {
   }
 
   paredDer() {
-    if (this.x == width / 10 - 1) {
+    if (this.x == width / escala - 1) {
       return true;
     }
   }
-  
-  contarVecinos(){
-    //cuenta cuando no esta en ninguna pared
-    if( !(this.paredDer() || this.paredIzq() || this.paredSup() || this.paredInf())){
-      let acum = 0
-      //completar
 
-      this.vecinos = acum
-    } else{
-      //contar para cuando esta en las paredes
-      //completar
+  contarVecinos() {
+    //cuenta cuando no esta en ninguna pared
+    if (
+      !(
+        this.paredDer() ||
+        this.paredIzq() ||
+        this.paredSup() ||
+        this.paredInf()
+      )
+    ) {
+      let acum = 0;
+      for (let i = -1; i <= 1; i++) {
+        for (let j = -1; j <= 1; j++) {
+          if (!(i == 0 && j == 0)) {
+            acum += celulas[this.x + i][this.y + j].viva;
+          }
+        }
+      }
+      this.vecinos = acum;
+    }
+    //contar para cuando esta en las esquinas
+    else if (this.paredSup() && this.paredDer()) {
+      //esquina superior derecha
+      this.vecinos =
+    } else if (this.paredSup() && this.paredIzq()) {
+      //esquina superior izquierda
+      this.vecinos =
+    } else if (this.paredInf() && this.paredIzq()) {
+      //esquina inf izquierda
+      this.vecinos =
+    } else if (this.paredInf() && this.paredDer()) {
+      //esquina inf der
+      this.vecinos =
+    } else if (this.paredSup()) {
+      //pared superior
+        this.vecinos =
+    } else if (this.paredIzq()) {
+      //pared izquierda
+        this.vecinos =
+    } else if (this.paredInf()) {
+      //pared inferior
+      this.vecinos =
+    } else if (this.paredDer()) {
+      //pared superior
+      this.vecinos =
     }
   }
-}
 
 let celulas;
 let columnas;
 let filas;
+let escala = 20;
 
 function setup() {
-  createCanvas(400, 400);
-  columnas = width / 10;
-  filas = height / 10;
+  noLoop();
+  createCanvas(600, 600);
+  columnas = width / escala;
+  filas = height / escala;
   //creo la matriz
   celulas = new Array(filas);
   for (let i = 0; i < celulas.length; i++) {
@@ -89,7 +111,7 @@ function setup() {
   //inicializo la matriz con células
   for (let i = 0; i < filas; i += 1) {
     for (let j = 0; j < columnas; j += 1) {
-      celulas[i][j] = new Celula(i, j, 10);
+      celulas[i][j] = new Celula(i, j, escala);
     }
   }
 }
@@ -97,11 +119,12 @@ function setup() {
 function draw() {
   background(0);
 
-  //cuento vecinos y muestro la matriz de celulas
+  //muestro la matriz de celulas
   for (let i = 0; i < filas; i += 1) {
     for (let j = 0; j < columnas; j += 1) {
-      celulas[i][j].contarVecinos()
+      celulas[i][j].contarVecinos();
       celulas[i][j].show();
+      //console.log(celulas[i][j].vecinos)
     }
   }
 }
