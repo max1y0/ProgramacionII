4269d4da0d669e98028e2511a14bff6226b2ccf3
Maxi <maximiliano.e.chaves@gmail.com>
2023-08-30 00:26:33 -0300

    Agrego ejercicios cola y pila

---
+++ cola.py
@@ -0,0 +1,19 @@
+class Cola:
+    def __init__(self,tamanio):
+        self.tamanio = tamanio
+        self.lista = []
+
+    def es_vacia(self):
+        #retorna verdadero si es vacía
+
+    def encolar(self, x):
+        #si no está llena, ingresa el elemento x al final de la lista
+
+    def consultar(self):
+        #si no está vacía, retorna el elemento que esta al comienzo de la lista
+
+    def desencolar(self):
+         #si no está vacía, elimina el elemento al inicio de la lista
+
+    sdef es_llena(self):
+        #retorna verdadero si el tamaño de la lista es igual al tamaño de la pila
