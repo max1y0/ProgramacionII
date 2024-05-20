533a59b3a9af658d30a66f98326e694c2e132fc5
Maxi <maximiliano.e.chaves@gmail.com>
2024-05-20 10:28:28 -0300

    Agrego mastermind sin terminar

--- mastermind.py
+++ mastermind.py
@@ -1,56 +1,52 @@
+
 import random
 
-#clase mastermind, completar:
+
 class mastermind:
     def __init__(self):
-        #completar
+        self.fichas = []
 
-    def inicializar(self):
-        #completar
+    def inicializar(self,lista):
+        self.fichas = lista
 
-    def inicializarRandom (self):
-        #completar
+    def inicializarRandom (self, dificultad):
+        if (dificultad.lower() == "facil"):
+            for _ in range(4): 
+                ficha = random.choice(["rojo","azul","verde","amarillo"])
+                while ficha in self.fichas:
+                    ficha = random.choice(["rojo","azul","verde","amarillo"])
+                self.fichas.append(ficha)
+        elif (dificultad.lower() == "medio"):
+            for _ in range(4): 
+                ficha = random.choice(["rojo","azul","verde","amarillo"])
+                self.fichas.append(ficha)
+        else:
+            for _ in range(4): 
+                self.fichas.append(random.choice(["rojo","azul","verde","amarillo","naranja","lila"]))
     
-    def chequeo (self,pos,color):
-        #completar
+    def chequeo (self,solucion):
+        fichas = self.fichas[:]
+        pista = ["errado","errado","errado","errado"]
+        for i in range (len(solucion)):
+            for j in range ( len(fichas) ):
+                # print(solucion[i],i,fichas[j],j)
+                if solucion[i] == fichas[j] and i == j:
+                    pista [i] = "toque"
+                    fichas[j] = " "
+                    break
+        for i in range (len(solucion)):
+            for j in range ( len(fichas) ):
+                # print(solucion[i],i,fichas[j],j)
+                if solucion[i] == fichas[j] and pista[i] != "toque":
+                    pista[i] = "roce"
+                    fichas[j] = " "
+                
+        pista.sort()
+        return pista
     
     def mostrar(self):
-        #completar
+        print(self.fichas)
     
                 
 
-#de aca en adolante NO TOCAR NADA
-juego = mastermind()
-print("Cuantos jugadores van a jugar? 1/2")
-jugadores = int(input())
-if (jugadores == 2):
-    print("Que el jugador 2 no vea tu combinacion secreta! \n")
-    juego.inicializar()
-else:
-    juego.inicializarRandom()
-
-intentos = 0
-listaPrevios = []
-while (intentos < 10):
-    solucion = []
-    for previos in listaPrevios:
-        print(previos)
-    solucion = input("\nIngresa tu combinación de colores separados por espacios: \n").split()
-
-    i = 0
-    pista = []
-    print("----------------------\nla pista según tu solución es: ")
-    while (i < 4):
-        pista.append(juego.chequeo(i,solucion[i]))
-        i+=1
-    pista.sort()
-    print (pista)
-    if (pista.count("toque")>=4):
-        print("\ntu solución era correcta!")
-        break
-    else:
-        print(f"\nSolución incorrecta, intenta de nuevo. Intento nro {intentos+1}")
-        intentos = intentos + 1
-    listaPrevios.append(solucion+pista)
-if intentos >=10:
-    print ("Perdiste el secreto era \n",juego.mostrar)
+#de aca  completar el codigo principal
