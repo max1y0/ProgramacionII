75cb8fd5da1672ed1b34dd458bac39e8868b7d10
Maxi <maximiliano.e.chaves@gmail.com>
2024-08-19 10:58:34 -0300

    Corrijo error nombre método

--- fraccion.py
+++ fraccion.py
@@ -2,7 +2,7 @@ class Fraccion:
 	def __init__(self,num,denom):
 		self.num = num
 		self.denom = denom
-	
+
 	def mostrar(self):
 		pass
 
@@ -11,7 +11,7 @@ class Fraccion:
 
 	def dividido( self, fraccion2):
 		pass
-	
+
 	def mas( self, fraccion2):
 		pass
 
@@ -29,22 +29,17 @@ while (opcion <5):
 
 	if (opcion ==1 ):
 		fraccion2 = Fraccion(int(input("Ingrese la segunda fraccion\n ")),int(input("--- \n ")))
-		print ("La suma es \n " + fraccion1.mas(fraccion2).show())
+		print ("La suma es \n " + fraccion1.mas(fraccion2).mostrar())
 
 	if (opcion ==2 ):
 		fraccion2 = Fraccion(int(input("Ingrese la segunda fraccion\n ")),int(input("--- \n ")))
-		print ("La resta es \n " + fraccion1.menos(fraccion2).show())
+		print ("La resta es \n " + fraccion1.menos(fraccion2).mostrar())
 
 	if (opcion ==3 ):
 		fraccion2 = Fraccion(int(input("Ingrese la segunda fraccion\n ")),int(input("--- \n ")))
-		print ("La multiplicación es \n " + fraccion1.por(fraccion2).show())
+		print ("La multiplicación es \n " + fraccion1.por(fraccion2).mostrar())
 
 	if (opcion ==4 ):
 		fraccion2 = Fraccion(int(input("Ingrese la segunda fraccion\n ")),int(input("--- \n ")))
-		print ("La división es \n " + fraccion1.dividido(fraccion2).show())
+		print ("La división es \n " + fraccion1.dividido(fraccion2).mostrar())
 	input()
-
-
-
-
-
